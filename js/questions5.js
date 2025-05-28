f2faba5206c057c0162c1c637c2ddd96df41e90e
sbanks11 <samielina2001@gmail.com>
2025-05-28 11:19:06 -0400

    Add files via upload

---
+++ js/questions5.js
@@ -0,0 +1,7 @@
+const questions = [
+    { question: "Do you enjoy seafood?", answers: ["Yes", "No", "Maybe", "Not sure"] },
+    { question: "Have you ever tried the Cheddar Bay Biscuits at Red Lobster?", answers: ["Yes", "No", "I want to", "Not applicable"] },
+    { question: "Do you believe Red Lobster offers a good variety of seafood dishes?", answers: ["Yes", "No", "Somewhat", "Not sure"] },
+    { question: "Would you consider dining at Red Lobster for a special occasion?", answers: ["Yes", "No", "Maybe", "Not applicable"] },
+    { question: "Do you think Red Lobster's prices are reasonable for the quality of food?", answers: ["Yes", "No", "Not sure", "I haven't checked"] },
+];
